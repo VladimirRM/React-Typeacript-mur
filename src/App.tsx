b8c6948cd9d53788043285637662b43e40fa135f
Vladimir RM <shvdrummer@gmail.com>
2023-07-17 15:34:02 +0300

    Start app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React,{FC} from 'react'
+
+const App:FC = () => {
+  return (
+    <div className='App'>
+      <div className="wrap">
+        <span className='heading'>Our pizza</span>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AddPizzaForm.tsx
@@ -0,0 +1,12 @@
+import React,{FC} from 'react'
+import "./styles.css"
+
+const AddPizzaForm:FC = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default AddPizzaForm
