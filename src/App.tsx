8a315fcb2e7307b7a3df013c290f1eea71735210
Vladimir RM <shvdrummer@gmail.com>
2023-07-18 10:55:16 +0300

    Get data from input.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React,{FC} from 'react'
+import AddPizzaForm from './components/AddPizzaForm'
+import './App.css'
 
 const App:FC = () => {
   return (
     <div className='App'>
       <div className="wrap">
         <span className='heading'>Our pizza</span>
+        <AddPizzaForm/>
       </div>
     </div>
   )
--- src/components/AddPizzaForm.tsx
+++ src/components/AddPizzaForm.tsx
@@ -1,11 +1,56 @@
-import React,{FC} from 'react'
+import React,{FC ,useState,ChangeEvent,FormEvent} from 'react'
 import "./styles.css"
 
+
+const initialState = {
+  title: '',
+  price: '',
+  img:'',
+}
+
 const AddPizzaForm:FC = () => {
+const [newPizza,setNewPizza] = useState<{title:string,price: string,img: string  }>(initialState)
+
+
+const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
+  console.log('handle change >>',e.target)
+     const {name,value} = e.target 
+     setNewPizza({
+      ...newPizza,
+      [name]: value
+     })
+}
+const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
+  e.preventDefault()
+  console.log('handle change >>',e.target)
+    
+}
+
   return (
-    <>
-      
-    </>
+    <form onSubmit={handleSubmit}>
+      <input
+       type="text"
+      name='title'
+      placeholder='название'
+      onChange={handleChange}
+      value = {newPizza.title}
+      />
+      <input
+       type="text"
+      name='price'
+      placeholder='стоимость'
+      onChange={handleChange}
+      value = {newPizza.price}
+      />
+      <input
+       type="text"
+      name='img'
+      placeholder='изображение'
+      onChange={handleChange}
+      value = {newPizza.img}
+      />
+      <button type='submit'>+ Add to menu</button>
+    </form>
   )
 }
 
